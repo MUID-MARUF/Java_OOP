596948040c239aa4a701426c91229c7abba699f6
Muid Hasan Maruf <121661366+MUID-MARUF@users.noreply.github.com>
2023-09-11 00:13:53 +0600

    Add files via upload

---
+++ Encapsulation.java
@@ -0,0 +1,18 @@
+package OOP;
+
+//to encapsulate, we use classes.
+
+class book{
+    String bookName;
+    int pages;
+    double price;
+}
+
+public class Encapsulation {
+
+    public static void main(String args[]){
+
+        System.out.println("Its a working java code to show an example of encapsulation.");
+    }
+    
+}
