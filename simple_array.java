aafa5e7ad3115edcd5899ec7e71490e8f23c1007
Muid Hasan Maruf <121661366+MUID-MARUF@users.noreply.github.com>
2023-09-20 23:08:27 +0600

    Add files via upload

---
+++ simple_array.java
@@ -0,0 +1,26 @@
+public class simple_array {
+
+    public static void main(String args[]){
+
+    int arr1[] = {1,2,3};
+    for(int i = 0; i<3; i++){
+        System.out.println(arr1[i]);
+    }
+
+    System.out.println();
+
+    String[] computers = {"HP", "Dell", "Apple"};
+
+    for(int j = 0; j<3; j++){
+        System.out.println(computers[j]);
+    }
+
+    System.out.println();
+    
+    double[] battery_life = {2.3, 4.6, 15.3};
+
+    for(int f = 0; f<3; f++){
+        System.out.println(battery_life[f]);
+    }
+    }
+}
